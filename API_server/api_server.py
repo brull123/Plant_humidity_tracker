caacc04449f303423a25a7598c7683f6b29a6b75
brull123 <50020349+brull123@users.noreply.github.com>
2023-04-20 07:43:37 +0200

    log loading

--- API_server/api_server.py
+++ API_server/api_server.py
@@ -2,11 +2,11 @@ from flask import Flask, json, request
 from flask_cors import CORS
 
 filename = "log.json"
-f =  open(filename, "r")
+f = open(filename, "r")
 try:
-  incoming_data = json.loads(f.read())
+    incoming_data = json.loads(f.read())
 except:
-  incoming_data = []
+    incoming_data = []
 
 api = Flask(__name__)
 CORS(api)
@@ -14,24 +14,31 @@ CORS(api)
 
 @api.route('/data', methods=['GET'])
 def get_data():
-  if not incoming_data:
-    return None
-  return json.dumps(incoming_data[-1])
+    if not incoming_data:
+      filename = "log.json"
+      f = open(filename, "r")
+      try:
+        incoming_data = json.loads(f.read())
+      except:
+        incoming_data = []
+        return None
+    return json.dumps(incoming_data[-1])
+
 
 @api.route('/download', methods=['GET'])
 def download_data():
-  if not incoming_data:
-    return None
-  return json.dumps(incoming_data)
+    if not incoming_data:
+        return None
+    return json.dumps(incoming_data)
+
 
 @api.route('/data_post', methods=['PUT'])
 def add_data():
-   if request.method == "PUT":
-    incoming = request.get_json()
-    incoming_data.append(incoming)
-    json.dump(incoming_data, open(filename,"w"), indent=4)
-    return "OK"
-
+    if request.method == "PUT":
+        incoming = request.get_json()
+        incoming_data.append(incoming)
+        json.dump(incoming_data, open(filename, "w"), indent=4)
+        return "OK"
 
 if __name__ == '__main__':
     api.run()
