b0896737ab39c8bf2b3ac4b8ad23493f4aa304c3
brull123 <50020349+brull123@users.noreply.github.com>
2023-06-08 10:57:59 +0200

    adding api gps functionality

--- API_server/api_server.py
+++ API_server/api_server.py
@@ -2,6 +2,7 @@ from flask import Flask, json, request
 from flask_cors import CORS
 import datetime
 
+# Plant humidity tracker
 filename_git = "log.json"
 filename = "/API/data/log_render.json"
 # filename = "log.json"
@@ -18,13 +19,31 @@ except:
     except:
         incoming_data = []
         print("Creating empty log")
-    json.dump(incoming_data, open(filename, "w"), indent=4)
+    json.dump(incoming_data, open(filename_git, "w"), indent=4)
+
+filename_git_gps = "log_gps.json"
+filename_gps = "/API/data/log_render_gps.json"
+# filename = "log_gps.json"
+try:
+    f = open(filename_gps, "r")
+    incoming_data_gps = json.loads(f.read())
+    print("Log found")
+except:
+    print("No log found")
+    try:
+        f = open(filename_git_gps, "r")
+        incoming_data_gps = json.loads(f.read())
+        print("Copying log from git")
+    except:
+        incoming_data_gps = []
+        print("Creating empty log")
+    json.dump(incoming_data_gps, open(filename_git_gps, "w"), indent=4)
 
 
 api = Flask(__name__)
 CORS(api)
 
-
+# Plant humidity tracker
 @api.route('/data', methods=['GET'])
 def get_data():
     global incoming_data
@@ -49,12 +68,24 @@ def add_data():
         json.dump(incoming_data, open(filename, "w"), indent=4)
         return "OK"
 
-@api.route('/data_post_2', methods=['POST'])
-def add_data_2():
+# GPS tracker
+@api.route('/data_post_gps', methods=['POST'])
+def add_data_gps():
+    now = json.dumps(datetime.datetime.now(), default=str)
     if request.method == "POST":
-        print("Received data")
-        print(request.json)
+        incoming = request.get_json()
+        print(incoming)
+        # incoming["time"] = now
+        incoming_data_gps.append(incoming)
         return "OK"
+
+@api.route('/data_get_gps', methods=['GET'])
+def get_data_gps():
+    if request.method == "GET":
+        if not incoming_data_gps:
+            return None
+        return json.dumps(incoming_data_gps[-1])
+
 
 
 if __name__ == '__main__':
